3d25c36397ac2c4da54939cb196e07addef2cd41
shanker9 <ssnani9@gmail.com>
2017-09-14 22:30:34 +0530

    Gridviewadded

---
+++ src/Components/GridView.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import TableRow from './TableRow.jsx';
+import styles from '../../styles/AppStyles.css'
+
+class GridView extends React.Component {
+
+    constructor(){
+        super();
+        this.groupedView = this.groupedView.bind(this);
+        this.normalview = this.normalview.bind(this);
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentDidUpdate() {
+    }
+
+    render() {
+        if(this.props.isGroupedView){
+            return this.groupedView();
+        }else{
+            return this.normalview();
+        }
+    }
+
+    groupedView() {
+        return (
+            <div>
+                <table className={styles.table}>
+                    <tbody className={styles.tableBody} >
+                        <div style={{ height: this.props.topDivHeight }}></div>
+                        {this.props.viewableData.map((item, i) =>
+                            <TableRow
+                                key={item.rowID}
+                                data={item.data}
+                                indexVal={item.data.swapId}
+                                dataUpdateHandler={this.props.selectionDataUpdateHandler}
+                                selectState={item.isSelected} />
+                        )}
+                        {this.props.groupedData.map((item, i) =>
+                            <TableRow
+                                key={item.rowID}
+                                data={item.data}
+                                indexVal={item.data.swapId}
+                                dataUpdateHandler={this.props.selectionDataUpdateHandler}
+                                selectState={item.isSelected} />
+                        )}
+                        <div style={{ height: this.props.bottomDivHeight }}></div>
+                    </tbody>
+                </table>
+            </div>
+
+        );
+    }
+
+    normalview() {
+        return (
+            <div>
+                <table className={styles.table}>
+                    <tbody className={styles.tableBody} >
+                        <div style={{ height: this.props.topDivHeight }}></div>
+                        {this.props.viewableData.map((item, i) =>
+                            <TableRow
+                                key={item.rowID}
+                                data={item.data}
+                                indexVal={item.data.swapId}
+                                dataUpdateHandler={this.props.selectionDataUpdateHandler}
+                                selectState={item.isSelected} />
+                        )}
+                        <div style={{ height: this.props.bottomDivHeight }}></div>
+                    </tbody>
+                </table>
+            </div>
+
+        );
+    }
+}
+
+
+export default GridView;
