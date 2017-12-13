48a0463fd8835174c33b094656882a508f39c989
shanker.a@euclidinnovations.com <ssnani9@gmail.com>
2017-12-13 19:27:56 +0530

    Changed if -else to switch case for message handler

--- src/Controllers/SubscriptionController.js
+++ src/Controllers/SubscriptionController.js
@@ -9,7 +9,69 @@ export default class SubscriptionController {
     }
 
 
+    defaultSubscriptionDataHandler1(message) {
+        if (message.c == 'group_begin') {
+            console.log(message.c);
+            return;
+        } else if (message.c == 'group_end') {
+            console.log(message.c);
+            this.parentControllerRef.updateUIWithDefaultViewData();
+            return;
+        }
+
+        let newData = message.data;
+        let rowKey = message.k;
+        let item = this.appDataModel.getDataFromDefaultData(rowKey);
+
+        if (item == undefined) {
+            this.appDataModel.addorUpdateRowData(rowKey, { "rowID": rowKey, "data": newData, "isSelected": false, "isUpdated": false });
+        } else {
+            this.appDataModel.addorUpdateRowData(rowKey, { "rowID": item.rowID, "data": newData, "isSelected": item.isSelected, "isUpdated": true });
+
+            if (this.appDataModel.getGroupedData() != undefined) {
+                this.parentControllerRef.updateRowDataInGroupedData(message);
+            }
+            this.parentControllerRef.updateUIRowWithData(newData,item.isSelected, item.rowID);
+        }
+    }
+
     defaultSubscriptionDataHandler(message) {
+
+        let messageType = message.c;
+
+        switch(messageType){
+            case 'group_begin':
+            console.log(message.c);
+            break;
+
+            case 'group_end':
+            console.log(message.c);
+            this.parentControllerRef.updateUIWithDefaultViewData();
+            break;
+
+            case 'sow':
+            case 'p':
+            let newData = message.data;
+            let rowKey = message.k;
+            let item = this.appDataModel.getDataFromDefaultData(rowKey);
+    
+            if (item == undefined) {
+                this.appDataModel.addorUpdateRowData(rowKey, { "rowID": rowKey, "data": newData, "isSelected": false, "isUpdated": false });
+            } else {
+                this.appDataModel.addorUpdateRowData(rowKey, { "rowID": item.rowID, "data": newData, "isSelected": item.isSelected, "isUpdated": true });
+    
+                if (this.appDataModel.getGroupedData() != undefined) {
+                    this.parentControllerRef.updateRowDataInGroupedData(message);
+                }
+                this.parentControllerRef.updateUIRowWithData(newData,item.isSelected, item.rowID);
+            }
+            break;
+
+            case Default:
+            console.log('OOF message received');
+        }
+
+
         if (message.c == 'group_begin') {
             console.log(message.c);
             return;
